97d4fc5cf2a653776f94d7197a213a5971cca3b3
PaolobrionesP <111026187+PaolobrionesP@users.noreply.github.com>
2023-05-02 17:32:06 -0500

    Init

---
+++ Actividad1/actividad1.vhd
@@ -0,0 +1,15 @@
+library ieee;
+
+entity actividad1 is
+    port (
+        
+    );
+end entity actividad1;
+
+architecture rtl of actividad1 is
+    
+begin
+    
+    
+    
+end architecture rtl;
